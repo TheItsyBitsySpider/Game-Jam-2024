4efb5085d788d82349fc8a9d3bf3897dab065864
Azia Bay-Asen <aziabayasen@gmail.com>
2024-10-03 16:03:51 -0500

    fix: variable name warning

--- cardgame/scripts/dialogue.gd
+++ cardgame/scripts/dialogue.gd
@@ -111,12 +111,12 @@ func _on_typewriter_timeout():
 	if len(dialogue.text) < len(text):
 		if not type_by_word:
 			var index = len(dialogue.text)
-			var char = text[index]
-			if char == '[':
+			var c = text[index]
+			if c == '[':
 				var tag = text.substr(index, text.find("]", index) - index + 1)
 				dialogue.text += tag
 				return _on_typewriter_timeout()
-			dialogue.text += char
+			dialogue.text += c
 		else:
 			var word = text.trim_prefix(dialogue.text).split(' ', false)[0]
 			dialogue.text += (" " if dialogue.text else "") + word
